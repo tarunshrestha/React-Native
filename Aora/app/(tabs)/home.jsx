9ce26c03f379254069f04be329e96086ff2ea6ef
tarun <tarunshrestha90@gmail.com>
2024-10-06 18:03:06 +0545

    text

--- Aora/app/(tabs)/home.jsx
+++ Aora/app/(tabs)/home.jsx
@@ -8,6 +8,7 @@ const Home = () => {
       {/* <Text className="text-center text-sm"></Text> */}
       <View className="bg-black-200 py-5 mt-2">
         <Text className="text-white text-center">Welcome Admin!</Text>
+        <Text className="text-sm">HI</Text>
       </View>
     </View>
   )
